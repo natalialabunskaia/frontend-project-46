0b7db744e8f6582dd7e34fa21b192d1f55e14330
Natalia Labunskaia <nlabunskya@gmail.com>
2025-03-18 18:05:07 +0400

    Add support for nested structures in gendiff and set stylish as the default formatter

--- README.md
+++ README.md
@@ -6,4 +6,4 @@
 
 [![Test Coverage](https://api.codeclimate.com/v1/badges/918cc80a627bf1e73bc8/test_coverage)](https://codeclimate.com/github/natalialabunskaia/frontend-project-46/test_coverage)
 
-[![asciicast](https://asciinema.org/a/MaUhQ1ygKK5zzbg2D6KXzFgLN.svg)](https://asciinema.org/a/MaUhQ1ygKK5zzbg2D6KXzFgLN)
+[![asciicast](https://asciinema.org/a/2C1K4lDECOobFL8mOFBxoMFpD.svg)](https://asciinema.org/a/2C1K4lDECOobFL8mOFBxoMFpD)
--- src/stylish.js
+++ src/stylish.js
@@ -14,25 +14,28 @@ const stringify = (coll, depth) => {
   return `{\n${lines.join('\n')}\n${str.slice(0, -2)}}`;
 };
 
-const stylish = (tree, depth = 1) => {
-  const result = tree.map((element) => {
-    const str = indent(depth);
-    switch (element.type) {
-      case 'nested':
-        return `${str}  ${element.key}: {\n${stylish(element.children, depth + 1)}\n  ${str}}`;
-      case 'unchanged':
-        return `${str}  ${element.key}: ${stringify(element.value, depth + 1)}`;
-      case 'deleted':
-        return `${str}- ${element.key}: ${stringify(element.value, depth + 1)}`;
-      case 'added':
-        return `${str}+ ${element.key}: ${stringify(element.value, depth + 1)}`;
-      case 'changed':
-        return `${str}- ${element.key}: ${stringify(element.value1, depth + 1)}\n${str}+ ${element.key}: ${stringify(element.value2, depth + 1)}`;
-      default:
-        return '';
-    }
-  });
-  return `${result.join('\n')}`;
+const stylish = (tree) => {
+  const iter = (node, depth = 1) => {
+    const result = node.map((element) => {
+      const str = indent(depth);
+      switch (element.type) {
+        case 'nested':
+          return `${str}  ${element.key}: {\n${iter(element.children, depth + 1)}\n  ${str}}`;
+        case 'unchanged':
+          return `${str}  ${element.key}: ${stringify(element.value, depth + 1)}`;
+        case 'deleted':
+          return `${str}- ${element.key}: ${stringify(element.value, depth + 1)}`;
+        case 'added':
+          return `${str}+ ${element.key}: ${stringify(element.value, depth + 1)}`;
+        case 'changed':
+          return `${str}- ${element.key}: ${stringify(element.value1, depth + 1)}\n${str}+ ${element.key}: ${stringify(element.value2, depth + 1)}`;
+        default:
+          return '';
+      }
+    });
+    return result.join('\n');
+  };
+  return `{\n${iter(tree)}\n}`;
 };
 
 export default stylish;

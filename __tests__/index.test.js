8c078a3f096aa183ced20de721148e5782a77877
Natalia Labunskaia <nlabunskya@gmail.com>
2025-03-22 16:37:38 +0400

    Work in progress: add new format "plain"

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,27 +1,48 @@
 import { readFileSync } from 'fs';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import gendiff from '../src/index.js';
+import genDiff from '../src/index.js';
+import plain from '../src/formatters/plain.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-test('gendiff should return correct difference for json files', () => {
+test('gendiff should return correct difference for json files (stylish)', () => {
   const file1Path = path.join(__dirname, '..', '__fixtures__', 'file1.json');
   const file2Path = path.join(__dirname, '..', '__fixtures__', 'file2.json');
-  const expectedPath = path.join(__dirname, '..', '__fixtures__', 'expected.txt');
+  const expectedPath = path.join(__dirname, '..', '__fixtures__', 'expected_stylish.txt');
 
-  const result = gendiff(file1Path, file2Path);
+  const result = genDiff(file1Path, file2Path);
   const expected = readFileSync(expectedPath, 'utf-8');
   expect(result).toBe(expected);
 });
 
-test('gendiff should return correct difference for yml/yaml files', () => {
+test('gendiff should return correct difference for yml/yaml files (stylish)', () => {
   const file1Path = path.join(__dirname, '..', '__fixtures__', 'file1.yml');
   const file2Path = path.join(__dirname, '..', '__fixtures__', 'file2.yaml');
-  const expectedPath = path.join(__dirname, '..', '__fixtures__', 'expected.txt');
+  const expectedPath = path.join(__dirname, '..', '__fixtures__', 'expected_stylish.txt');
 
-  const result = gendiff(file1Path, file2Path);
+  const result = genDiff(file1Path, file2Path);
+  const expected = readFileSync(expectedPath, 'utf-8');
+  expect(result).toBe(expected);
+});
+
+test('gendiff should return correct difference for json files (plain)', () => {
+  const file1Path = path.join(__dirname, '..', '__fixtures__', 'file1.json');
+  const file2Path = path.join(__dirname, '..', '__fixtures__', 'file2.json');
+  const expectedPath = path.join(__dirname, '..', '__fixtures__', 'expected_plain.txt');
+
+  const result = genDiff(file1Path, file2Path, plain);
+  const expected = readFileSync(expectedPath, 'utf-8');
+  expect(result).toBe(expected);
+});
+
+test('gendiff should return correct difference for yml/yaml files (plain)', () => {
+  const file1Path = path.join(__dirname, '..', '__fixtures__', 'file1.yml');
+  const file2Path = path.join(__dirname, '..', '__fixtures__', 'file2.yaml');
+  const expectedPath = path.join(__dirname, '..', '__fixtures__', 'expected_plain.txt');
+
+  const result = genDiff(file1Path, file2Path, plain);
   const expected = readFileSync(expectedPath, 'utf-8');
   expect(result).toBe(expected);
 });
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import parse from './parser.js';
 import treeOfDiff from './builderTreeOfDiff.js';
-import stylish from './stylish.js';
+import stylish from './formatters/stylish.js';
+import plain from './formatters/plain.js';
 
 const genDiff = (filepath1, file1Path2, format = 'stylish') => {
   const coll1 = parse(filepath1);
   const coll2 = parse(file1Path2);
-  return stylish(treeOfDiff(coll1, coll2));
+  switch (format) {
+    case 'stylysh':
+      return stylish(treeOfDiff(coll1, coll2));
+    case 'plain':
+      return plain(treeOfDiff(coll1, coll2));
+    default:
+      return 'Unknown format';
+  }
 };
 
 export default genDiff;
